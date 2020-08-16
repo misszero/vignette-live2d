e3676a1b78169bb0cdcb88674903408bb1bdf7e6
unknown <n20gaming2000@gmail.com>
2020-08-17 07:00:07 +0800

    fix errors

--- osu.Framework.Live2D.Tests/Visual/TestSceneMotionQueue.cs
+++ osu.Framework.Live2D.Tests/Visual/TestSceneMotionQueue.cs
@@ -10,24 +10,24 @@ namespace osu.Framework.Live2D.Tests.Visual
         private void load()
         {
             AddLabel("Basic Motion");
-            AddStep("enqueue motion", () => Sprite.StartMotion("", 0, CubismAsset.MotionType.Base));
+            AddStep("enqueue motion", () => Sprite.StartMotion("", 0, MotionType.Base));
             AddAssert("check motion", () => Sprite.IsMoving);
             AddStep("stop queue", () => Sprite.StopMotion());
 
             // The first motion is popped off from the queue and is played in the next update frame
             AddLabel("Queued Motions");
-            AddRepeatStep("enqueue motions", () => Sprite.StartMotion("", 0, CubismAsset.MotionType.Base), 5);
+            AddRepeatStep("enqueue motions", () => Sprite.StartMotion("", 0, MotionType.Base), 5);
             AddAssert("check queue count", () => Sprite.BaseMotionsQueued  == 4);
             AddRepeatStep("skip motions", () => Sprite.NextMotion(), 4);
             AddAssert("check queue if empty", () => Sprite.BaseMotionsQueued == 0);
 
             AddLabel("Forced Motion");
-            AddRepeatStep("enqueue motions", () => Sprite.StartMotion("", 0, CubismAsset.MotionType.Base), 5);
-            AddStep("enqueue forced motion", () => Sprite.StartMotion("", 1, CubismAsset.MotionType.Base, true));
+            AddRepeatStep("enqueue motions", () => Sprite.StartMotion("", 0, MotionType.Base), 5);
+            AddStep("enqueue forced motion", () => Sprite.StartMotion("", 1, MotionType.Base, true));
             AddAssert("check queue if empty", () => Sprite.BaseMotionsQueued == 0);
 
             AddLabel("Looping Motion");
-            AddStep("enqueue looping motion", () => Sprite.StartMotion("", 2, CubismAsset.MotionType.Base, true, true));
+            AddStep("enqueue looping motion", () => Sprite.StartMotion("", 2, MotionType.Base, true, true));
             AddWaitStep("wait", 30);
             AddAssert("check motion", () => Sprite.IsMoving);
             AddStep("stop motion", () => Sprite.StopMotion());
