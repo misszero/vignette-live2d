9ced7da54713b40cdb7a23f7dbc0834044ca94ff
Capuccino <chinodesuuu@gmail.com>
2020-09-12 10:30:19 +0000

    create seperate deploy script for GHPR
    Signed-off-by: Capuccino <chinodesuuu@gmail.com>

---
+++ deploy-nuget.ps1
@@ -0,0 +1,19 @@
+$request = "https://api.nuget.org/v3/registration5-gz-semver2/osu.framework.live2d/index.json"
+$entries = Invoke-WebRequest $request | ConvertFrom-Json | Select-Object -expand items
+
+$year = (Get-Date).Year
+$monthDay = [int]([String](Get-Date).Month + [String](Get-Date).Day)
+$current = $entries[0].items.catalogEntry.version.Split(".")
+$revision = 0
+
+if ( ([int]($current[0]) -eq $year) -and ([int]($current[1]) -eq $monthDay) ) {
+    $revision = [int]($current[2]) + 1
+}
+
+if ($monthDay.length -eq 2) {
+    $monthDay = "0" + $monthDay
+}
+
+$version = [string]::Format("{0}.{1}.{2}", [string]$year, [string]$monthDay, [string]$revision)
+& dotnet.exe pack ./osu.Framework.Live2D/osu.Framework.Live2D.csproj -c Release -o output /p:Version=$version
+& dotnet.exe nuget push ./output/osu.Framework.Live2D.$version.nupkg --api-key $env:NUGET_API_KEY --skip-duplicate --no-symbols true
