995b0229baff84a829e59f3533187c338b546087
Nitrous <n20gaming2000@gmail.com>
2020-12-18 16:50:28 +0800

    update math and utils

--- Vignette.Application.Live2D/Utils/CubismMath.cs
+++ Vignette.Application.Live2D/Utils/CubismMath.cs
@@ -2,11 +2,48 @@
 // Licensed under MIT. See LICENSE for details.
 
 using System;
+using osuTK;
 
 namespace Vignette.Application.Live2D.Utils
 {
     public static class CubismMath
     {
         public static float EaseSine(float t) => Math.Clamp(0.5f - 0.5f * MathF.Cos(MathF.PI * t), 0, 1);
+
+        public static float DegreesToRadian(float degrees) => (degrees / 180.0f) * MathF.PI;
+
+        public static float RadianToDegrees(float radians) => (radians * 180.0f) * MathF.PI;
+
+        public static float DirectionToRadian(Vector2 from, Vector2 to)
+        {
+            float q1, q2, ret;
+
+            q1 = MathF.Atan2(to.Y, to.X);
+            q2 = MathF.Atan2(from.Y, from.X);
+            ret = q1 - q2;
+
+            while (ret < -MathF.PI)
+                ret += MathF.PI * 2.0f;
+
+            while (ret > MathF.PI)
+                ret -= MathF.PI * 2.0f;
+
+            return ret;
+        }
+
+        public static float DirectionToDegrees(Vector2 from, Vector2 to)
+        {
+            float radian, degree;
+
+            radian = DirectionToRadian(from, to);
+            degree = RadianToDegrees(radian);
+
+            if ((to.X - from.X) > 0)
+                degree = -degree;
+
+            return degree;
+        }
+
+        public static Vector2 RadianToDirection(float totalAngle) => new Vector2(MathF.Sin(totalAngle), MathF.Cos(totalAngle));
     }
 }
---
+++ Vignette.Application.Live2D/Utils/CubismUtils.cs
@@ -0,0 +1,74 @@
+﻿// Copyright 2020 - 2021 Vignette Project
+// Licensed under MIT. See LICENSE for details.
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace Vignette.Application.Live2D.Utils
+{
+    public static class CubismUtils
+    {
+        public static IntPtr Align(IntPtr ptr, int offset)
+        {
+            IntPtr aligned;
+            int totalOffset = 0;
+
+            do
+            {
+                aligned = IntPtr.Add(ptr, totalOffset);
+                totalOffset++;
+            } while ((ulong)aligned % (ulong)offset != 0);
+
+            return aligned;
+        }
+
+        public static string[] PointerToStringArray(IntPtr ptr, int count)
+        {
+            string[] ret = new string[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                var element = Marshal.PtrToStructure<IntPtr>(ptr);
+                ptr = IntPtr.Add(ptr, IntPtr.Size);
+                ret[i] = Marshal.PtrToStringAnsi(element);
+            }
+
+            return ret;
+        }
+
+        public static float[] PointerToFloatArray(IntPtr ptr, int count)
+        {
+            float[] ret = new float[count];
+            Marshal.Copy(ptr, ret, 0, count);
+            return ret;
+        }
+
+        public static short[] PointerToShortArray(IntPtr ptr, int count)
+        {
+            short[] ret = new short[count];
+            Marshal.Copy(ptr, ret, 0, count);
+            return ret;
+        }
+
+        public static int[] PointerToIntArray(IntPtr ptr, int count)
+        {
+            int[] ret = new int[count];
+            Marshal.Copy(ptr, ret, 0, count);
+            return ret;
+        }
+
+        public static byte[] PointerToByteArray(IntPtr ptr, int count)
+        {
+            byte[] ret = new byte[count];
+            Marshal.Copy(ptr, ret, 0, count);
+            return ret;
+        }
+
+        public static IntPtr[] PointerToPointerArray(IntPtr ptr, int count)
+        {
+            IntPtr[] ret = new IntPtr[count];
+            Marshal.Copy(ptr, ret, 0, count);
+            return ret;
+        }
+    }
+}
