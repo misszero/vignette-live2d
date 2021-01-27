425be7c8dae720b135655209680b4f87e8137d97
Nitrous <n20gaming2000@gmail.com>
2021-01-27 17:27:26 +0800

    modify licensing headers
    This is to be in-line with Live2D's Open Software License Agreement found here: http://live2d.com/eula/live2d-open-software-license-agreement_en.html

--- Vignette.Application.Live2D/Json/ICubismJsonSetting.cs
+++ Vignette.Application.Live2D/Json/ICubismJsonSetting.cs
@@ -1,5 +1,7 @@
-﻿// Copyright 2020 - 2021 Vignette Project
-// Licensed under MIT. See LICENSE for details.
+﻿// Copyright (c) Vignette Project.
+// Licensed under MIT. Please see LICENSE for more details.
+// This software implements Live2D. Copyright (c) Live2D Inc. All Rights Reserved.
+// License for Live2D can be found here: http://live2d.com/eula/live2d-open-software-license-agreement_en.html
 
 namespace Vignette.Application.Live2D.Json
 {
